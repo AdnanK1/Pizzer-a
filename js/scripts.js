58a9f75aba46984a6689ca918ae2cc5e019ddc36
Adnan Kibe <adnangitonga@gmail.com>
2020-03-02 10:54:46 +0300

    Added a dropdown box.

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,4 @@ $(document).ready(function(){
     $(".button1").click(function(){
         $(".form1").toggle();
       });
-      $(".button2").click(function(){
-        $(".form2").toggle();
-      });
-      $(".button3").click(function(){
-        $(".form3").toggle();
-      });
-      $(".button4").click(function(){
-        $(".form4").toggle();
-      });
-      $(".button5").click(function(){
-        $(".form5").toggle();
-      });
-      $(".button6").click(function(){
-        $(".form6").toggle();
-      });
 });
