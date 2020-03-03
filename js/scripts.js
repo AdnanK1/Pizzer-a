f3fe2ddb4d450fffc0d5ad686da1d81900f1b7be
Adnan Kibe <adnangitonga@gmail.com>
2020-03-03 09:10:50 +0300

    Added functionality to the button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,91 @@
-
-
-
-
 $(document).ready(function(){
     $(".button1").click(function(){
-        $(".form1").toggle();
+        $(".form1").toggle(); 
       });
 });
+$("document").ready(function(){
+  function person (name,email,phone){
+      this.name = name;
+      this.email = email;
+      this.phone = phone;
+
+  }
+
+      person.prototype.profile = function(){
+       $("#name").text("Your name is "+this.name)
+       $("#email").text("Your Email address is "+this.email)
+       $("#phone").text("Your Phone number is "+this.phone)
+      }
+
+      $("#profile").click(function(){
+          var name = $(".name").val();
+          var email =$(".Email").val();
+          var phone = $(".phone").val();
+          var location = $(".location").val();
+          var user = new person(name,email,phone,location);
+          user.profile()
+
+      })
+
+  function order(size,toppings,number,delivery,type){
+      this.size = size;
+      this.toppings =toppings;
+      this.number = number;
+      this.delivery = delivery;
+      this.type = type;
+  }
+
+  order.prototype.price = function(){
+      let price =0;
+      let Total =0;
+      if (this.size === "Large"){
+       price =1100;
+       Total = price + 300;
+       Total = Total * this.number;
+
+      }
+
+      else if (this.size === "Medium"){
+          price = 800;
+          Total = price + 300;
+          Total =Total * this.number;
+      }
+
+      else if(this.size ==="small"){
+          price=500
+          Total = price + 300;
+          Total = Total * this.number;
+
+      }
+
+      else alert("Enter your sizes ")
+      var delivery=prompt("Would like it hand delivered or you would like to proceed to our restaurant ? YES/NO")
+      if(delivery === "YES"){
+          alert("You'll have to add ksh.300 to your full price ")
+          Total = Total + 300;
+           var location =prompt("Please enter your location")
+           $("#location").text("Your location is "+location)
+
+      }
+
+      else if(delivery === "NO"){
+          alert("We are expecting you soon");
+
+      }
+      else alert('Wrong choice');
+      $("#details").text("Your pizza will be ready shortly,The total price is  "+Total +" Your order is  " +this.type +" And your  pizza size is  "+this.size);
+  
+  }
+
+   $(".button7").click(function(){
+       var type = $("#type").val()
+       var size= $("#size").val();
+       var toppings = $(".toppings").val();
+       var number = $(".number").val();
+       var delivery = $(".delivery").val();
+        var myOrder = new order(size,toppings,number,delivery,type);
+        myOrder.price()
+
+   });   
+
+})
